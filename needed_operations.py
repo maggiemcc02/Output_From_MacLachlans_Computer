f6c3d5bb5deaf49e9c083b0bb2b1ddfdd9267f75
Maggie McCarthy <108485831+maggiemcc02@users.noreply.github.com>
2023-06-14 12:11:20 -0230

    Testing Python on Multiple Files
    I have two files that I am hoping to run in python. One depends on the other. The success of running these codes will help me move on to saving larger sets of code as multiple files

---
+++ needed_operations.py
@@ -0,0 +1,25 @@
+# operations needed for my test file
+
+
+def add_numbers(a, b):
+
+    return a + b
+
+
+def sub_numbers(a, b):
+
+    return a - b
+
+
+def multiply_numbers(a, b):
+
+    return a * b
+
+def divide_numbers(a, b):
+
+    return a / b
+
+
+def exponent_numbers(a, b):
+
+    return a ** b
